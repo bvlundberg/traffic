05c488cda794b4f617bbde9272353af8c3e1d625
unknown <Brandon@REMOTE_PRG2.DL2012.LOCAL>
2015-12-07 21:10:23 -0800

    Most of implementation set

--- traffic.cpp
+++ traffic.cpp
@@ -4,42 +4,45 @@
 	Date: 		2 December 2015
 */
 
-#define NUM_CARS 50
-#define PASS_PER_GREEN 5
+#define NUM_CARS 60
+#define DIRECTIONS 4
+#define CARS_NORTH 15
+#define CARS_EAST 15
+#define CARS_SOUTH 15
+#define CARS_WEST 15
+#define MAX_CARS_PASSING 5
 
+// 0: North
+// 1: East
+// 2: South
+// 3: West
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <mutex>
+#include <queue>
 
-//srand(time(NULL));
-//rand()
-int main()
-{
-	pthread_t intersectionThread;
-	pthread_t carsThreads[NUM_CARS];
+using namespace std;
+// Some global variables
+time_t timer;
+mutex northlock;
+mutex eastlock;
+mutex southlock;
+mutex westlock;
 
-}
 
 class Intersection{
-		private:
-		Lock lock;
-		Cond carsPass;
-		// The name of the queue is the direction the cars are headed
-		Queue<Car> North;
-		Queue<Car> East;
-		Queue<Car> South;
-		Queue<Car> West;
-
+	public:
 		int arrivalCount;
 		int passCount;
-		int passPerGreen; // n can pass per green light
+		int directionFlag;
+		// 0: North/South
+		// 1: East/West
 
-		
-	public:
 		Intersection()
 		{
-			BuildRandomCars();
 			OpenStreet();
 		};
 		~Intersection() {};
@@ -55,17 +58,6 @@ class Intersection{
 		} 			
 		void EnterIntersection();	// Called by cars
 
-	private:
-		void BuildRandomCars()
-		{
-			// Get random seed (direction of a new car) 
-			// srand(something);
-			sleep(rand());
-			for(int i = 0; i < 50; i++)
-			{
-				OriginalCars.push(new Car(rand()));
-			}
-		}
 		void WaitForCar();
 		void PrintIntersectionStats();
 
@@ -73,12 +65,137 @@ class Intersection{
 };
 
 class Car{
-	private:
-		int direction;	// North: 0, East: 1, South: 2, West: 3
 	public:
-		Car(int d){
-			direction = d;
-		}
-	private:
+		int direction;	// North: 0, East: 1, South:2, West: 3
+		int entrytime;
+		int exittime;
+	
+		Car(){}
 		void donePassing();
 };
+
+queue<Car> northqueue;
+queue<Car> eastqueue;
+queue<Car> southqueue;
+queue<Car> westqueue;
+
+void ProcessCar(Car c){
+	c.exittime = difftime(timer,time(NULL));
+}
+
+void GoCarsGo(queue<Car> a, queue<Car> b){
+	int carCount = 0;
+	while(a.size() != 0 && b.size() != 0 && carCount < MAX_CARS_PASSING){
+		ProcessCar(a.pop());
+		ProcessCar(b.pop());
+		carCount++;
+	}
+}
+
+void *ManageIntersection(){
+	Intersection intersection;
+	while(intersection.passCount < NUM_CARS){
+		// Allow cars in that direction to pass
+		switch(intersection.directionFlag){
+			case 0: 
+				lock(northlock, southlock);
+				GoCarsGo(northqueue, southqueue);
+				unlock(northlock, southlock);
+				break;
+			case 1:
+				lock(eastlock, westlock);
+				GoCarsGo(eastqueue, westqueue);
+				unlock(eastlock, westlock);
+				break;
+			default:
+				cout << "Bad direction" << endl;
+				break;
+		}
+		// Stop cars in that direction from passings
+		intersection.directionFlag 	~= intersection.directionFlag;
+		sleep(100);
+	}
+	pthread_exit(NULL);
+}
+//lock(mutex1, mutex2)
+void NorthQueueAddCar(Car c){
+	lock(northlock);
+	northqueue.push(c);
+	unlock(northlock);
+}
+
+void *ManageNorth(){
+	for(int i = 0; i < CARS_NORTH; i++){
+		Car c;
+		c.direction = 0;
+		c.entrytime = difftime(timer,time(NULL));
+		NorthQueueAddCar(c);
+	}
+	pthread_exit(NULL);
+}
+
+void EastQueueAddCar(Car c){
+	lock(eastlock);
+	eastqueue.push(c);
+	unlock(eastlock);
+}
+
+void *ManageEast(){
+	for(int i = 0; i < CARS_EAST; i++){
+		Car c;
+		c.direction = 0;
+		c.entrytime = difftime(timer,time(NULL));
+		EastQueueAddCar(c);
+	}
+	pthread_exit(NULL);
+}
+
+void SouthQueueAddCar(Car c){
+	lock(southlock);
+	southqueue.push(c);
+	unlock(southlock);
+}
+
+void *ManageSouth(){
+	for(int i = 0; i < CARS_SOUTH; i++){
+		Car c;
+		c.direction = 0;
+		c.entrytime = difftime(timer,time(NULL));
+		SouthQueueAddCar(c);
+	}
+	pthread_exit(NULL);
+}
+
+void WestQueueAddCar(Car c){
+	lock(westlock);
+	westqueue.push(c);
+	unlock(westlock);
+}
+
+void *ManageWest(){
+	for(int i = 0; i < CARS_WEST; i++){
+		Car c;
+		c.direction = 0;
+		c.entrytime = difftime(timer,time(NULL));
+		WestQueueAddCar(c);
+	}
+	pthread_exit(NULL);
+}
+
+int main(){
+	// Global variables for the intersection thread and each direction thread
+	pthread_t intersectionThread;
+	pthread_t directionThreads[DIRECTIONS];
+	//Start timer
+	time(&timer);
+	std::cout << "Start at time 0" << std::endl;
+	// Create intersection thread and open the street
+	pthread_create(intersectionThread, NULL, ManageIntersection, NULL);
+
+	// Start each car thread after a random sleep time
+	pthread_create(&directionThreads[0], NULL, ManageNorth, NULL);
+	pthread_create(&directionThreads[1], NULL, ManageEast, NULL);
+	pthread_create(&directionThreads[2], NULL, ManageSouth, NULL);
+	pthread_create(&directionThreads[3], NULL, ManageWest, NULL);
+	pthread_exit(NULL);
+}
